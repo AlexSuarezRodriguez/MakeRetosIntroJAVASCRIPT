91a2fd591038e5f76193c39ea50bc8d9bf0b2f90
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 13:08:07 -0500

    programa con error de salida

---
+++ arreglando_el_error.js
@@ -0,0 +1,14 @@
+/*
+Objetivo: Corregir el siguiente programa para obtener el resultado esperado.
+El JavaScript actual en el archivo arreglando-el-error.js es el siguiente:*/
+
+var name;
+var jobTitle = "Director de Marketing"
+
+console.log("El " + jobTitle +" se llama " + name);
+//El resultado actual del programa es:
+//El Director de Marketing se llama undefined
+
+//El resultado esperado es:
+//El Director de Marketing se llama Felipe
+//Explica por qué el programa no funciona como se espera y corrígelo.
