727478e8566f49a9826b64c145bc1e91cf5708c3
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 13:03:21 -0500

    programa para presentar una persona

---
+++ presentadoPersona.js
@@ -0,0 +1,6 @@
+/*
+Presentando personas
+Objetivo: Escribir un programa que al ejecutarlo desde la consola de tu computador, usando node, imprima el siguiente texto:
+*/
+var nombre="alexander suarez rodriguez",edad=28,pais="colombia"
+console.log(nombre +" tiene "+edad+" años y vive en "+pais)
