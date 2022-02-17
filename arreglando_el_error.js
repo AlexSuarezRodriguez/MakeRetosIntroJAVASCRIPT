d9c09e26bceae153d3c8947a8ab380727c4d8cd7
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 13:13:52 -0500

    error de salida corregido

--- arreglando_el_error.js
+++ arreglando_el_error.js
@@ -2,7 +2,7 @@
 Objetivo: Corregir el siguiente programa para obtener el resultado esperado.
 El JavaScript actual en el archivo arreglando-el-error.js es el siguiente:*/
 
-var name;
+var name = "alexander";
 var jobTitle = "Director de Marketing"
 
 console.log("El " + jobTitle +" se llama " + name);
@@ -12,3 +12,5 @@ console.log("El " + jobTitle +" se llama " + name);
 //El resultado esperado es:
 //El Director de Marketing se llama Felipe
 //Explica por qué el programa no funciona como se espera y corrígelo.
+
+//al no asignarle un dato a la variable en ese instante la variable esta vacia o void, por ende al mostrarla despues por consola no se sabe que typo de dato es.
