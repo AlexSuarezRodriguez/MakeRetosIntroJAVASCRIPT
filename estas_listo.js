8c75d3d1c55e07a58411e8f8d56e146da4824a8c
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 15:17:30 -0500

    programa que evalua si la ocndicon de un nombre se cumple usando  operador ternario

---
+++ estas_listo.js
@@ -0,0 +1,6 @@
+/*
+Objetivo: 
+Escribe un programa que imprima en la consola Está listo, si el nombre de la persona es María o Pedro. Si las condiciones no se cumplem, el programa debe finalizar.
+*/
+var nombre ="maria";
+(nombre==="maria"||"pedro")?console.log("esta listo"):false;
