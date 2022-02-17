bcf25b7e64301869708e3c906acf30928914fb56
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 14:43:53 -0500

    programa para evaluar un numero mayor que diez

---
+++ mayor_que_diez.js
@@ -0,0 +1,7 @@
+/*
+Objetivo: escribir un programa que imprima "Es mayor que 10" si el número asignado a la variable number es mayor que 10. 
+*/
+var numero=12;
+if(numero>10){
+  console.log("el numero es mayor que diez")
+}
