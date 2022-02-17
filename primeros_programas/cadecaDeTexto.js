e38e4215f5356e6ca8d3da5e87469b257505e011
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 12:35:59 -0500

    programa con 3 diferetes metodes de mostrar cadena de caracteres

---
+++ primeros_programas/cadecaDeTexto.js
@@ -0,0 +1,7 @@
+/*
+Objetivo: 
+Utilizar tres formas diferentes de escribir una cadena de texto (string), para obtener el siguiente resultado en la consola de node Mi programa de JavaScript. 
+*/
+console.log('Mi programa de JavaScript')
+console.log("Mi programa de JavaScript")
+console.log("Mi "+"programa "+"de "+"JavaScript")
