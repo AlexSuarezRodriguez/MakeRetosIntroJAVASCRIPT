0abd93799f02d4d6e3ce240c1257f92dae49667b
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 16:06:02 -0500

    programa que imprime 50 veces un mensaje usando cicllo while

---
+++ aprendiendo_ciclos.js
@@ -0,0 +1,9 @@
+/*
+Objetivo: Crear un programa que imprima en la consola 50 veces el texto Estoy aprendiendo.
+Cada vez que se imprime el texto, debe aparecer al inicio el número de veces que se ha impreso:
+*/
+let variable=0;
+while(variable <50){
+  console.log((variable+1),"estoy aprendiendo");
+  variable++;
+}
