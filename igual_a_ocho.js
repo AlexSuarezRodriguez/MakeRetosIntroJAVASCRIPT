626e8d044b9c2cf6404b47e7e1b17176a1ef23d7
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 14:49:44 -0500

    uso de operador ternario

--- igual_a_ocho.js
+++ igual_a_ocho.js
@@ -3,8 +3,7 @@ Objetivo:
 escribir un programa que imprima El número es igual a 8 si el número asignado a la variable number es 8, e imprima No es 8 si es diferente de 8.
 */
 var numero=7;
-if (numero ===8){
-  console.log("el numero es igual a 8")
-}else{
+//operador ternario
+(numero ===8)?
+  console.log("el numero es igual a 8"):
   console.log("el numero no es igual a ocho")
-}
