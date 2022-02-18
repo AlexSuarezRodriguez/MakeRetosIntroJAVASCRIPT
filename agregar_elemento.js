a9858d9c7aada8d94e3018c87649b9144866a946
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 21:03:00 -0500

    agregando elemento al arreglo y recorriendolo

---
+++ agregar_elemento.js
@@ -0,0 +1,10 @@
+/*
+Objetivo: Crear un programa que inserte en el siguiente arreglo la cadena de texto "Soy el último elemento", e imprima en consola el resultado esperado.
+Arreglo inicial:
+[ "Primero", "Segundo", "Tercero", "Cuarto"]
+*/
+let arreglo=[ "Primero", "Segundo", "Tercero", "Cuarto"];
+arreglo.push("Soy el último elemento");
+for(let i=0;i<arreglo.length;i++){
+  console.log(arreglo[i]);
+}
