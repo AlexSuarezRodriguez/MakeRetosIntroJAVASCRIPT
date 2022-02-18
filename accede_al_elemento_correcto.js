5fd41b0766a8084448b062b8e66c659838f115a9
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 20:57:29 -0500

    programa para acceder a posiciones especificas

---
+++ accede_al_elemento_correcto.js
@@ -0,0 +1,7 @@
+/*
+Objetivo: Crea un programa que acceda a los elementos ubicados en la primera y tercera posición del siguiente arreglo, e imprima el resultado esperado.
+Arreglo:
+[1, 2, 3, 4, 5]
+*/
+let arreglo=[1, 2, 3, 4, 5]
+console.log(`el dato de la primera posicion es ${arreglo[1]} y el de la segunda es ${arreglo[3]}`)
