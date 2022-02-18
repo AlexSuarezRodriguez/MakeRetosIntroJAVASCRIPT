0d279fb2849747ae1adad1e0736fcdb946c6a2c3
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 20:58:36 -0500

    filtrado de datos en las posiciones especificas

--- accede_al_elemento_correcto.js
+++ accede_al_elemento_correcto.js
@@ -3,5 +3,8 @@ Objetivo: Crea un programa que acceda a los elementos ubicados en la primera y t
 Arreglo:
 [1, 2, 3, 4, 5]
 */
-let arreglo=[1, 2, 3, 4, 5]
-console.log(`el dato de la primera posicion es ${arreglo[1]} y el de la segunda es ${arreglo[3]}`)
+let arreglo=[1, 2, 3, 4, 5],datos=[]
+for (let i=0;i<arreglo.length;i++){
+  (i%2!==0)?datos.push(arreglo[i]):false;
+}
+console.log(`el dato en la primera posicion es ${datos[0]} el de la segunda posicion es ${datos[1]}`)
