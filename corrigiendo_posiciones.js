89a891bad9b826d62b1712d15fd82bf3ce065d86
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 21:15:34 -0500

    corrigiendo posiciones, se agrega los elementos del nuevo arreglo

---
+++ corrigiendo_posiciones.js
@@ -0,0 +1,9 @@
+/*
+Objetivo: Escribe un programa que inserte cada uno de los elementos del arreglo nuevoArreglo, en el arreglo resultante del reto anterior arr, e imprima cada uno de los elementos en la consola.
+
+var arr = ["Primero", "Segundo", "Tercero", "Cuarto", "Soy el último elemento"];
+var newArray = ["Quinto", "Sexto"];
+*/
+let arr = ["Primero", "Segundo", "Tercero", "Cuarto", "Soy el último elemento"],newArray = ["Quinto", "Sexto"];
+arr.splice(4,0,newArray[0],newArray[1])
+console.log(arr)
