dcb2586338eeb30025a04271d2386178cfbb81c8
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 20:39:08 -0500

    recorrer un arreglo con foreach

--- imprimirElArreglo.js
+++ imprimirElArreglo.js
@@ -2,13 +2,9 @@
 Objetivo: Crear un programa que imprima en la consola todos los elmentos del siguiente arreglo:
 ["Simón", "Natalia", "Nayib", "Diana", "Germán"]
 */
+
+//usando foreach el cual abrebia la manera de recorrer un arreglo
 const array1 = ["Simón", "Natalia", "Nayib", "Diana", "Germán"];
-//la manera de como se abrebio un foreach.se ejecuta un for por la cantidad de posiciones del arreglo 
-//luego se hace el llamado de la funcion donde el dato de la posicion se pasara como argumento y luego 
-//este se retorna y ese dato es el que se imprime o muestra
-function imprimir(elemento){
-  return elemento
-}
-for(i=0;i<array1.length;i++){
-  console.log(imprimir(array1[i]))
-}
+array1.forEach(function(element){
+  console.log(element)
+});
