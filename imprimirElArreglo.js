0ed30478024736c4e5304f809c482b24a9bd10e4
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 20:31:08 -0500

    recorriendo arreglo

---
+++ imprimirElArreglo.js
@@ -0,0 +1,8 @@
+/*
+Objetivo: Crear un programa que imprima en la consola todos los elmentos del siguiente arreglo:
+["Simón", "Natalia", "Nayib", "Diana", "Germán"]
+*/
+array=["Simón", "Natalia", "Nayib", "Diana", "Germán"];
+for(i=0;i<array.length;i++){
+  console.log(array[i])
+}
