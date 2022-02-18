2485adbc636b382744c330d0a185fc09741a91a7
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 20:32:15 -0500

    manera explicita de programacion funcional para recorre un arreglo

--- imprimirElArreglo.js
+++ imprimirElArreglo.js
@@ -2,7 +2,13 @@
 Objetivo: Crear un programa que imprima en la consola todos los elmentos del siguiente arreglo:
 ["Simón", "Natalia", "Nayib", "Diana", "Germán"]
 */
-array=["Simón", "Natalia", "Nayib", "Diana", "Germán"];
-for(i=0;i<array.length;i++){
-  console.log(array[i])
+const array1 = ["Simón", "Natalia", "Nayib", "Diana", "Germán"];
+//la manera de como se abrebio un foreach.se ejecuta un for por la cantidad de posiciones del arreglo 
+//luego se hace el llamado de la funcion donde el dato de la posicion se pasara como argumento y luego 
+//este se retorna y ese dato es el que se imprime o muestra
+function imprimir(elemento){
+  return elemento
+}
+for(i=0;i<array1.length;i++){
+  console.log(imprimir(array1[i]))
 }
