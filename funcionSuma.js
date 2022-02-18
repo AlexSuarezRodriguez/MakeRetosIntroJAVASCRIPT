cd13b2b152b3179e59c9779502fc93265dd75858
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-17 19:02:50 -0500

    programa con diseño de funcion para sumar 2 numeros

---
+++ funcionSuma.js
@@ -0,0 +1,15 @@
+/*
+Objetivo: Escribir una función llamada suma que reciba dos números y retorne su suma.
+Valida los siguientes casos con tu programa:
+console.log(sum(10, 20));  // 30
+console.log(sum(10, 10));  // 20
+console.log(sum(40, 60));  // 100
+console.log(sum(50, 200));  // 250
+*/
+function sum(numero1,numero2){
+  return numero1+numero2;
+}
+console.log(sum(10, 20));  // 30
+console.log(sum(10, 10));  // 20
+console.log(sum(40, 60));  // 100
+console.log(sum(50, 200));  // 250
