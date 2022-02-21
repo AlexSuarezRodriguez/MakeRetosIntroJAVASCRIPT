30c4c9ce8210c2e1e663da8f587125102ec609b5
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-21 11:11:19 -0500

    programa donde se itera con las llaves del objeto para acceder a su valor,creando un arreglo aparte con las llaves

--- recipe.js
+++ recipe.js
@@ -1,11 +1,12 @@
 let recipe={
-  LecheEnPolvo:"1 litro",
+  LecheEnPolvo:"1 litro",//las llaves si van separadas por espacios debe ir entre comillas como si duera un string
   chocolateEnBarra:"3 barras",
   azucar:"3 gramos",
   //saludo:function(){console.log("hola")}
 }
-//console.log(Object.keys(recipe).length)// con esta manera se obtiene la longuitud del objeto pero cuenta atributos y metodos
-console.log(recipe)
-console.log(recipe.LecheEnPolvo)
-console.log(recipe.chocolateEnBarra)
-console.log(recipe.azucar)
+let llaves = Object.keys(recipe);
+//console.log(recipe[llaves[0]])
+
+for (let i=0;i<llaves.length;i++){
+  console.log(`agrega ${recipe[llaves[i]]} de ${llaves[i]}`)//no funciona con la notacion punto para acceder al valor de la llave si no notacio llave (objeto[llave])
+}
