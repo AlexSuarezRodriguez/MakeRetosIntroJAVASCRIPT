4db433a25bc39f1d85cc37290d522442c1c68d80
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-20 19:18:33 -0500

    cambio en la manera de obtener el string con las primeras letras en mayuscula

--- capitalize.js
+++ capitalize.js
@@ -9,13 +9,11 @@ console.log(capitalize("make it real")); // "Make It Real"
 
 */
 function capitalize (cadena){
-  let datonuevo=[];
-  cadena=cadena.split(" ")
-  for(i=0;i<cadena.length;i++){
-  datonuevo.push(cadena[i][0].toUpperCase()+cadena[i].substr(1)); 
+  cadena=cadena.split(" ")// convertir el dato de entrada en un arreglo
+  for(i=0;i<cadena.length;i++){ // ejecutar el for de acuerdo a la longuitud del arreglo
+  cadena.splice(i,1,cadena[i][0].toUpperCase()+cadena[i].substr(1));//reemplazar cada posicion con la priemra letra convertida a mayuscula y luego concatenando los siguientes datos
 }
-return datonuevo.join(" ");
+return cadena.join(" ");// al final se retorna es arreglo obtenido pero uniendolos y con ello pasandolo a una cadena de texto separada por espacios
 }
-
 console.log(capitalize("pedro perez")); // "Pedro Perez"
 console.log(capitalize("make it real")); // "Make It Real"
