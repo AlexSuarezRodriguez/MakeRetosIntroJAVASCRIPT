2280c5178158fb7cb7450f8d18dffce472e4cb1d
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-20 21:00:57 -0500

    primer programa con objetos iterables, imprimir los datos del objeto

---
+++ recipe.js
@@ -0,0 +1,11 @@
+let recipe={
+  LecheEnPolvo:"1 litro",
+  chocolateEnBarra:"3 barras",
+  azucar:"3 gramos",
+  //saludo:function(){console.log("hola")}
+}
+//console.log(Object.keys(recipe).length)// con esta manera se obtiene la longuitud del objeto pero cuenta atributos y metodos
+console.log(recipe)
+console.log(recipe.LecheEnPolvo)
+console.log(recipe.chocolateEnBarra)
+console.log(recipe.azucar)
